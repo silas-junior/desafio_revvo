63fda410b9cdf84d24974029488e4438ce376aac
silas-junior <silas.pereirajunior04@outlook.com>
2025-02-24 16:15:37 -0300

    add code scope to api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,3 +1,56 @@
 <?php
 
-echo "Api working...";
+// VAI REMOVER QUERY PARAMS E RETORNAR SOMENTE '/api/courses/{id}'
+$uri = trim(explode('?', $_SERVER['REQUEST_URI'])[0], '/');
+$method = $_SERVER['REQUEST_METHOD'];
+
+// echo json_encode($uri);
+// die();
+
+$basePath = '/api';
+
+// VAI RETIRAR A BARRA(/) DO $basePath E VAI VERIFICAR SE A POSIÇÃO DA REFERÊNCIA(api) DENTRO DE $uri É IGUAL A 0
+// OU SEJA, COMO A $uri POR PADRÃO DEVE SER: api/courses, ENTÃO A POSIÇÃO DA REFERÊNCIA DE FATO VAI SER 0
+// MAS CASO SEJA INFORMADA ALGO DIFERENTE DE: api/courses, POR EXEMPLO: teste/api/courses VAI RETORNAR QUE A ROTA NÃO EXISTE
+if(strpos($uri, trim($basePath, '/')) === 0) {
+   $path = substr($uri, strlen($basePath));
+} else {
+   $path = $uri;
+}
+
+// VAI VERIFICAR SE O $path POSSUI *OPCIONALMENTE* ALGUM NÚMERO DEPOIS DE "courses/"
+if (preg_match('/^courses(?:\/(\d+))?$/', $path, $matches)) {
+   $id = null;
+
+   if(!empty($matches[1])) {
+      $id = intval($matches[1]);
+   }
+
+
+   switch ($method) {
+      case 'GET':
+         if(!$id) {
+            echo "Buscando todos...";
+         } else {
+            echo "Buscando somente um...";
+         }
+         break;
+      case 'POST':
+         echo "Criando...";
+         break;
+      case 'PUT':
+         echo "Atualizando...";
+         break;
+      case 'DELETE':
+         echo "Deletando...";
+         break;
+      
+      default:
+         http_response_code(404);
+         echo json_encode(['message' => 'Invalid request method']);
+         break;
+   }
+} else {
+   http_response_code(404);
+   echo json_encode(["error" => "Rota não encontrada"]);
+}
