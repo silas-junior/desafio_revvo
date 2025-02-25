8ec82ae9e0ab8e75469c9de7666136e020abce5d
silas-junior <silas.pereirajunior04@outlook.com>
2025-02-25 09:26:50 -0300

    applying extension '.php' on test_db

--- backend/database/test_db.php
+++ backend/database/test_db.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    echo "✅ Conexão bem-sucedida!";
+    echo "✅ Conexão bem-sucedida!\n";
 } catch (PDOException $e) {
     echo "❌ Erro na conexão: " . $e->getMessage() . "\n";
 }
