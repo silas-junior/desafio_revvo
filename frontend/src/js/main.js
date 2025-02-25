acad670e6276bf990a9b508afd306e2027dac304
silas-junior <silas.pereirajunior04@outlook.com>
2025-02-25 20:31:05 -0300

    sctructure of list courses on frontend

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -2,7 +2,7 @@ async function fetchCursos() {
    const response = await fetch("/api/courses");
    const courses = await response.json();
 
-   const list = document.getElementById("course-list");
+   const list = document.getElementById("list_courses");
    list.innerHTML = "";
    courses.forEach(course => {
        const li = document.createElement("li");
