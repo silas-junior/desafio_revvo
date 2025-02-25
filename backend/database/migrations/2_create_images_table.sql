ff63610db45d99ca939d022374c781d93963c3de
silas-junior <silas.pereirajunior04@outlook.com>
2025-02-25 08:09:16 -0300

    changing relationshipment between courses and images

--- backend/database/migrations/2_create_images_table.sql
+++ backend/database/migrations/2_create_images_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS images (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT references courses(id),
     content VARCHAR(255) NOT NULL,
     extension TEXT
 );
--- backend/database/migrations/3_create_course_images_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS course_images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    course_id INT references courses(id),
-    image_id INT references courses(id)
-);
