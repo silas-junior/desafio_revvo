64d1188297cdd7fa92abaced58d3b1471bd95b07
silas-junior <silas.pereirajunior04@outlook.com>
2025-02-25 22:25:00 -0300

    including modal to show info when user enter first time on page

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,27 @@
+function showModal(modal_id) {
+    let modal = document.getElementById(`${modal_id}`);
+
+    modal.style.display = "block";
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function closeModal(modal_id) {
+    let modal = document.getElementById(`${modal_id}`);
+
+    modal.style.display = "none";
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 async function fetchCursos() {
    const response = await fetch("/api/courses");
    const courses = await response.json();
